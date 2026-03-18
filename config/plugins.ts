c51a148c40681fee15bfbe03cb85fca5cf60b15b
divyeshv-rango <divyesh.v@rango.design>
2026-03-18 15:22:40 +0530

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,15 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true, // 👈 enables the playground
+      depthLimit: 7,
+      amountLimit: 100,
+    }
+  }
+});
 
 export default config;
