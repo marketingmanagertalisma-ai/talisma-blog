154bbebd5212b6d7afc79bc657a242484429bf55
divyeshv-rango <divyesh.v@rango.design>
2026-03-18 15:39:45 +0530

    add cloudinary config

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,11 +5,21 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
-      playgroundAlways: true, // 👈 enables the playground
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
     }
-  }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  },
 });
 
 export default config;
